b2ce4fcd5c692264fb2ceced192ff3827e7ddd86
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-09-03 02:19:49 -0600

    refactored all of the components to be class components if you need props then you also need a constructor you need to call render and extend React.Component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,15 @@
+import { render } from "@testing-library/react";
 import React from "react";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">Home</a>
-      <a href="#about">About</a>
-    </nav>
-  );
+class NavBar extends React.Component {
+  render() {
+    return (
+      <nav>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+      </nav>
+    );
+  }
 }
 
 export default NavBar;
